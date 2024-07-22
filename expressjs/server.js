00b2a09d50283b34690606589f067f3eb58678bd
V <truyenhaunhan@gmail.com>
2024-07-22 20:38:25 +0700

    Add global rate limiter.

--- expressjs/server.js
+++ expressjs/server.js
@@ -10,11 +10,24 @@ const dotenv = require('dotenv'); // Loads environment variables from the .env f
 dotenv.config({
     path: '.env',
 }); // https://stackoverflow.com/questions/44915758/node-process-env-variable-name-returning-undefined
+
 const express = require('express'),
+    { rateLimit } = require('express-rate-limit'),
     cors = require('cors');
+
+// Global rate limiter.
+const limiter = rateLimit({
+    // 300 requests per 15 minutes.
+    windowMs: 15 * 60 * 1000,
+    limit: 300,
+    standardHeaders: 'draft-7',
+    legacyHeaders: false,
+});
 
 // Express configurations.
 const app = express();
+app.set('trust proxy', 1); // Trust nginx proxy so rate limiter won't complaint.
+app.use(limiter);
 app.use(cors());
 app.use(express.json());
 app.use(
